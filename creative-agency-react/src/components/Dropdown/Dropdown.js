d57be1d62cfcfc107dc66343c919465f0ef80a57
Risad212 <hafez.risad@gmail.com>
2022-06-04 09:56:05 +0600

    almost done

--- creative-agency-react/src/components/Dropdown/Dropdown.js
+++ creative-agency-react/src/components/Dropdown/Dropdown.js
@@ -3,19 +3,19 @@ import React, { useState } from 'react';
 const Dropdown = ({ elem }) => {
     const [option, setOption] = useState()
     
-    console.log(option)
+    console.log(elem)
     return (
         <tr>
             <td>{elem?.name}</td>
-            <td>{elem?.name}</td>
+            <td>{elem?.email}</td>
             <td>{elem?.service}</td>
             <td>{elem?.discription}</td>
             <td className='text-right select' >
                 <select name="status" id="" onChange={(e) => setOption(e.target.value)} style={{color: option == 'Panding'? 'red': option == 'Done'? 'green': option == 'on Going'? 'yellow': '#000'}}>
                     <option value="Status" style={{color: '#000'}}>Status</option>
-                    <option value="Panding" style={{color: 'red'}}>Panding</option>
-                    <option value="Done" style={{color: 'green'}}>Done</option>
-                    <option value="on Going" style={{color: 'yellow'}}>On Going</option>
+                    <option value="Panding" style={{color: '#FF4545'}}>Panding</option>
+                    <option value="Done" style={{color: '#009444'}}>Done</option>
+                    <option value="on Going" style={{color: '#FFBD3E'}}>On Going</option>
                 </select>
             </td>
         </tr>
--- creative-agency-react/src/components/Services/Services.js
+++ creative-agency-react/src/components/Services/Services.js
@@ -1,30 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './Service.css';
-import  services1 from '../../images/icons/service1.png'
-import  services2 from '../../images/icons/service2.png'
-import  services3 from '../../images/icons/service3.png'
 import { useNavigate } from 'react-router-dom';
 
 const Services = () => {
     const [service, setService] = useState()
-    const servicesData = [
-        {
-            icon: services1,
-            title: 'Web & Mobile design',
-            disc: 'We craft stunning and amazing web UI, using a well drrafted UX to fit your product.'
-        },
-        {
-            icon: services2,
-            title: 'Graphic design',
-            disc: 'Amazing flyers, social media posts and brand representations that would make your brand stand out.'
-        },
-        {
-            icon: services3,
-            title: 'Web development',
-            disc: 'With well written codes, we build amazing apps for all platforms, mobile and web apps in general.'
-        }
-    ]
-
     // navigate to order dashbaord
     const navigate = useNavigate()
 
