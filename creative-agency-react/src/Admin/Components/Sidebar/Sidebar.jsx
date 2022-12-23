cb623e3759b0b7cbd8a98c7ca0e58018109a213d
Risad212 <hafez.risad@gmail.com>
2022-12-23 12:21:08 +0600

    getting error

--- creative-agency-react/src/Admin/Components/Sidebar/Sidebar.jsx
+++ creative-agency-react/src/Admin/Components/Sidebar/Sidebar.jsx
@@ -3,6 +3,9 @@ import './Sidebar.css';
 import LineStyleIcon from '@material-ui/icons/LineStyle';
 import TimelineIcon from '@material-ui/icons/Timeline';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import PeopleIcon from '@material-ui/icons/People';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import InventoryIcon from '@material-ui/icons/Inventory';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
@@ -21,10 +24,10 @@ const Sidebar = () => {
               <div className="sidebarMenu">
                  <h3 className="sidebarTitle">Quick Menu</h3>
                  <ul className="sidebarList">
-                    <Link to="/user" className='link'><li className="sidebarListItem"><LineStyleIcon className='sidebarIcon'/>Users</li></Link>
-                    <Link to="/product" className='link'><li className="sidebarListItem"><TimelineIcon className='sidebarIcon'/>Products</li></Link>
-                    <li className="sidebarListItem"><TrendingUpIcon className='sidebarIcon'/>Transactions</li>
-                    <li className="sidebarListItem"><TrendingUpIcon className='sidebarIcon'/>Reports</li>
+                    <Link to="/user" className='link'><li className="sidebarListItem"><PeopleIcon className='sidebarIcon'/>Users</li></Link>
+                    <Link to="/newuser" className='link'><li className="sidebarListItem"><PersonAddIcon className='sidebarIcon'/>Add User</li></Link>
+                    <Link to="/product" className='link'><li className="sidebarListItem"><InventoryIcon className='sidebarIcon'/>Products</li></Link>
+                    <Link to="/newproduct" className='link'><li className="sidebarListItem"><TimelineIcon className='sidebarIcon'/>Add Products</li></Link>
                  </ul>
               </div>
               {/*-- menu --*/}
